8f17d341dd6d34e5050080a51f327738468c4696
aleanajla <aleanajla12@gmail.com>
2023-09-24 18:21:52 +0700

    init project
    setup + routing basic

---
+++ tbs/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout/>} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ tbs/src/pages/Layout.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Layout() {
+    return (
+        <>
+            <div className="text-3xl text-gray-200">
+                <p className="text-primary">HELLLOOOOO</p>
+            </div>
+        </>
+    )
+}
