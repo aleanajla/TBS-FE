588a078b40ee64bba611341a6ddaacfce1275fdf
aleanajla <aleanajla12@gmail.com>
2023-10-08 17:18:41 +0700

    slicing tabs

--- tbs/src/pages/Homepage/index.jsx
+++ tbs/src/pages/Homepage/index.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Layout from "../../components/Layouts/Layout";
 import Menu from "../../components/Menu";
 import FilterSearch from "../../components/FilterSearch";
+import Tabs from "../../components/Tabs";
 
 export default function Homepage() {
     return(
         <Layout>
             <Menu/>
+            <Tabs/>
         </Layout>
 
     )
