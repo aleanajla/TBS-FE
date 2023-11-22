1ee659548a52e0e57b3a6d706109e0f29dbccc69
anidadewip <anidadewip@gmail.com>
2023-11-22 16:51:06 +0700

    update menu TC

--- tbs/src/components/Menu/menuTC.jsx
+++ tbs/src/components/Menu/menuTC.jsx
@@ -4,24 +4,22 @@ import { Link } from "react-router-dom";
 export function MenuTC() {
   return (
     <>
-      <Link to={"/myBooking"}>
-        <button
-          className="w-32 flex flex-col items-center rounded-md p-2"
-          style={{ backgroundColor: "#38358F" }}
-        >
-          <img src="/images/homepage_mybooking_logo.svg" />
-          <p className="w-24 text-center">STID</p>
-        </button>
-      </Link>
-      <Link to={"/myBooking"}>
-        <button
-          className="w-32 flex flex-col items-center rounded-md p-2"
-          style={{ backgroundColor: "#38358F" }}
-        >
-          <img src="/images/homepage_mybooking_logo.svg" />
-          <p className="w-24 text-center">Transport Order</p>
-        </button>
-      </Link>
+      <div className="flex flex-col">
+        <Link to={"/transportOder"}>
+          <button className="flex flex-col items-center rounded-md px-4 pt-4 min-h-[180px] gap-4" style={{ backgroundColor: "#38358F" }}>
+            <img src="/images/transportOrdeLogo.svg" />
+            <p className="w-24 text-center font-medium">Transport <br /> Order </p>
+          </button>
+        </Link>
+      </div>
+      <div className="w-full flex flex-col" >
+        <Link to={"/STID"}>
+          <button className="flex flex-col items-center rounded-md px-4 pt-4 min-h-[180px] gap-4" style={{ backgroundColor: "#38358F" }}>
+            <img src="/images/STIDlogo.svg" />
+            <p className="w-24 text-center font-medium"> STID <br />  </p>
+          </button>
+        </Link>
+      </div>
     </>
   );
 }
